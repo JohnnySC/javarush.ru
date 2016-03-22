32a9fd91440ddfb0aca79726b2aaf8e6fc36cf6c
Hovhannes Asatryan <johnnysc091@gmail.com>
2016-03-22 20:32:35 +0300

    level04/lesson10/task03 solution

---
+++ level04/lesson10/task03/Solution.java
@@ -0,0 +1,34 @@
+package com.javarush.test.level04.lesson10.task03;
+
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+
+/* Хорошего не бывает много
+Ввести с клавиатуры строку и число N.
+Вывести на экран строку N раз используя цикл while.
+Пример ввода:
+абв
+2
+Пример вывода:
+абв
+абв
+*/
+
+public class Solution
+{
+    public static void main(String[] args) throws Exception
+    {
+        BufferedReader reader = new BufferedReader(new InputStreamReader(System.in));
+        String name = reader.readLine();
+        String sN = reader.readLine();
+        int NN = Integer.parseInt(sN);
+
+        int i=0;
+        while (i<NN)
+        {
+            System.out.println(name);
+            i++;
+        }
+
+    }
+}
