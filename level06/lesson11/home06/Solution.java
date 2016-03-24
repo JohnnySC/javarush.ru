82d21b8a4b43c54caebebf73a60ea5185cc46c7d
Hovhannes Asatryan <johnnysc091@gmail.com>
2016-03-24 11:54:34 +0300

    level06/lesson11/home06 solution

---
+++ level06/lesson11/home06/Solution.java
@@ -0,0 +1,21 @@
+package com.javarush.test.level06.lesson11.home06;
+
+/* KissMyShinyMetalAss
+Создай класс с именем KissMyShinyMetalAss. Создай объект этого класса, выведи его на экран.
+*/
+
+public class Solution
+{
+    public static void main(String[] args)
+    {
+        KissMyShinyMetalAss kiss1 = new KissMyShinyMetalAss();
+        System.out.println(kiss1);
+    }
+
+    public static class KissMyShinyMetalAss
+    {
+        static String name="Kiss My Shiny Metal Ass";
+
+    }
+
+}
