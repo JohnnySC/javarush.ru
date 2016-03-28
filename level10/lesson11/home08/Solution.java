53bd43c2ecfa31cbaa90f1756b502c9932df5e98
Hovhannes Asatryan <johnnysc091@gmail.com>
2016-03-28 22:56:54 +0300

    level10/lesson11/home08 solution

---
+++ level10/lesson11/home08/Solution.java
@@ -0,0 +1,46 @@
+package com.javarush.test.level10.lesson11.home08;
+
+import java.util.ArrayList;
+import java.util.List;
+
+/* Массив списков строк
+Создать массив, элементами которого будут списки строк. Заполнить массив любыми данными и вывести на их экран.
+*/
+
+public class Solution
+{
+    public static void main(String[] args)
+    {
+        ArrayList<String>[] arrayOfStringList =  createList();
+        printList(arrayOfStringList);
+    }
+
+    public static ArrayList<String>[] createList()
+    {
+        ArrayList<String> nl1 = new ArrayList<String>();
+        ArrayList<String> nl2 = new ArrayList<String>();
+        nl1.add("first thing");
+        nl2.add("second one");
+        ArrayList[] group = new ArrayList[2];
+
+        group[0]=nl1;
+       group[1]=nl2;
+
+
+
+
+
+        return group;
+    }
+
+    public static void printList(ArrayList<String>[] arrayOfStringList)
+    {
+        for (ArrayList<String> list: arrayOfStringList)
+        {
+            for (String s : list)
+            {
+                System.out.println(s);
+            }
+        }
+    }
+}
