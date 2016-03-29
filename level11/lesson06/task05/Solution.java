a4af949fc5d313d748aa00dda01afad8bb78bee6
Hovhannes Asatryan <johnnysc091@gmail.com>
2016-03-29 12:26:57 +0300

    level11/lesson06/task05 solution

---
+++ level11/lesson06/task05/Solution.java
@@ -0,0 +1,67 @@
+package com.javarush.test.level11.lesson06.task05;
+
+/* ИТ-компания
+Написать девять классов: Worker(сотрудник), Clerk (клерк), IT (ИТ-специалист), Programmer(программист), ProjectManager(менеджер проекта), CTO(технический директор), HR(рекрутер), OfficeManager(офис-менеджер), Cleaner (уборщик).
+Унаследовать программиста, менеджера проекта и технического директора от ИТ-специалиста.
+Унаследовать рекрутера, уборщика и офис-менеджера от клерка.
+Унаследовать клерка и ИТ-специалиста от сотрудника.
+*/
+
+public class Solution
+{
+    public static void main(String[] args)
+    {
+    }
+
+    public class Worker
+    {
+        String firstName;
+        String secondName;
+        boolean sex;
+        int age;
+        int salary;
+
+    }
+
+    public class Clerk extends Worker
+    {
+        boolean minWage;
+
+    }
+
+    public class IT extends Worker
+    {
+        boolean itType;
+
+    }
+
+    public class Programmer extends IT
+    {
+
+    }
+
+    public class ProjectManager  extends IT
+    {
+
+    }
+
+    public class CTO  extends IT
+    {
+
+    }
+
+    public class OfficeManager extends Clerk
+    {
+
+    }
+
+    public class HR extends Clerk
+    {
+
+    }
+
+    public class Cleaner extends Clerk
+    {
+
+    }
+}
