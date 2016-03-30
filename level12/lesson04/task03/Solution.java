ffb1be049278f0679e3dc0b0f68245d25f3e71d1
Hovhannes Asatryan <johnnysc091@gmail.com>
2016-03-30 18:11:53 +0300

    level12/lesson04/task03 solution

---
+++ level12/lesson04/task03/Solution.java
@@ -0,0 +1,30 @@
+package com.javarush.test.level12.lesson04.task03;
+
+/* Пять методов print с разными параметрами
+Написать пять методов print с разными параметрами.
+*/
+
+public class Solution
+{
+    public static void main(String[] args)
+    {
+
+    }
+
+    public static void print(int a)
+    {
+       System.out.println(a);
+    }
+        public static void print(Integer a)
+    {
+        System.out.println(a);
+    }
+    public static void print(String s)
+    {System.out.println(s);}
+    public static void print(int s,int f){
+        System.out.println(s+f);}
+        public static void print(String s, int j){
+    System.out.println(s+j);}
+        }
+
+
