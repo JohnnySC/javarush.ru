781cdaa4a60ec6edf55ef19888fcd2c319e386fd
Hovhannes Asatryan <johnnysc091@gmail.com>
2016-04-01 22:02:47 +0300

    level14/lesson08/home01 class SuspensionBridge

---
+++ level14/lesson08/home01/SuspensionBridge.java
@@ -0,0 +1,12 @@
+package com.javarush.test.level14.lesson08.home01;
+
+/**
+ * Created by Johnny on 19.07.2014.
+ */
+public class SuspensionBridge implements Bridge
+{
+    public int getCarsCount()
+    {
+        return 7;
+    }
+}
