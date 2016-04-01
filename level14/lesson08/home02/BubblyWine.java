fe7e9a535717d7ae2f313b52341700c7fd0ebef0
Hovhannes Asatryan <johnnysc091@gmail.com>
2016-04-01 22:04:06 +0300

    level14/lesson08/home02 class BubblyWine

---
+++ level14/lesson08/home02/BubblyWine.java
@@ -0,0 +1,12 @@
+package com.javarush.test.level14.lesson08.home02;
+
+/**
+ * Created by Johnny on 19.07.2014.
+ */
+public class BubblyWine extends Wine
+{
+    public String getHolidayName()
+    {
+        return "Новый год";
+    }
+}
