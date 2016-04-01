db9582c6975fa57335aa85f6124730e95f02dd03
Hovhannes Asatryan <johnnysc091@gmail.com>
2016-04-01 22:06:22 +0300

    level14/lesson08/home05 class Keyboard

---
+++ level14/lesson08/home05/Keyboard.java
@@ -0,0 +1,12 @@
+package com.javarush.test.level14.lesson08.home05;
+
+/**
+ * Created by Johnny on 19.07.2014.
+ */
+public class Keyboard implements CompItem
+{
+    public String getName()
+    {
+        return "Keyboard";
+    }
+}
