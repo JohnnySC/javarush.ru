09f6eeb97f5401e976261f3881cadcf7337c3bfc
Hovhannes Asatryan <johnnysc091@gmail.com>
2016-04-01 22:03:51 +0300

    level14/lesson08/home02 class Drink

---
+++ level14/lesson08/home02/Drink.java
@@ -0,0 +1,12 @@
+package com.javarush.test.level14.lesson08.home02;
+
+/**
+ * Created by Johnny on 19.07.2014.
+ */
+public abstract class Drink
+{
+    public  void taste()
+    {
+        System.out.println("Вкусно");
+    }
+}
