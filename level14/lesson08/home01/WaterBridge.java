c0f4f7f43e746f4f895289a6161faf55aaf272f1
Hovhannes Asatryan <johnnysc091@gmail.com>
2016-04-01 22:03:04 +0300

    level14/lesson08/home01 class WaterBridge

---
+++ level14/lesson08/home01/WaterBridge.java
@@ -0,0 +1,12 @@
+package com.javarush.test.level14.lesson08.home01;
+
+/**
+ * Created by Johnny on 19.07.2014.
+ */
+public class WaterBridge implements Bridge
+{
+    public int getCarsCount()
+    {
+        return 5;
+    }
+}
