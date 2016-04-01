2b5e86782056a9833d8d9ceff5c3e0281c192432
Hovhannes Asatryan <johnnysc091@gmail.com>
2016-04-01 22:03:41 +0300

    level14/lesson08/home02 class Wine

---
+++ level14/lesson08/home02/Wine.java
@@ -0,0 +1,12 @@
+package com.javarush.test.level14.lesson08.home02;
+
+/**
+ * Created by Johnny on 19.07.2014.
+ */
+public class Wine extends Drink
+{
+    public String getHolidayName()
+    {
+        return "День рождения";
+    }
+}
