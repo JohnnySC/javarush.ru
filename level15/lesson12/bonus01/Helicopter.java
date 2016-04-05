9359fc589cf5dbb85c176314fccacc6b9125cd5e
Hovhannes Asatryan <johnnysc091@gmail.com>
2016-04-05 11:49:23 +0300

    level15/lesson12/bonus01 class Helicopter

---
+++ level15/lesson12/bonus01/Helicopter.java
@@ -0,0 +1,14 @@
+package com.javarush.test.level15.lesson12.bonus01;
+
+
+/**
+ * Created by johnny on 09.02.16.
+ */
+public class Helicopter implements Flyable
+{
+    @Override
+    public void fly()
+    {
+
+    }
+}
