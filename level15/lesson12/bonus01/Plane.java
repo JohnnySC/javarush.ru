4c96671ae5b1692f04c247e30afd07f0929f0bdc
Hovhannes Asatryan <johnnysc091@gmail.com>
2016-04-05 11:49:08 +0300

    level15/lesson12/bonus01 class Plane

---
+++ level15/lesson12/bonus01/Plane.java
@@ -0,0 +1,19 @@
+package com.javarush.test.level15.lesson12.bonus01;
+
+/**
+ * Created by johnny on 09.02.16.
+ */
+public class Plane implements Flyable
+{
+    int n;
+    @Override
+    public void fly()
+    {
+
+    }
+
+    public Plane(int n)
+    {
+        this.n = n;
+    }
+}
